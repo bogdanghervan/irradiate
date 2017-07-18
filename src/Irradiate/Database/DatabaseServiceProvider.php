a4243e2bcb9169b80845c20fe246b458296925ef
Andrei Petcu <mihai@adoreme.com>
2017-07-18 23:45:03 +0300

    Updated serviceProvider for Laravel 5.4

--- src/Irradiate/Database/DatabaseServiceProvider.php
+++ src/Irradiate/Database/DatabaseServiceProvider.php
@@ -3,12 +3,16 @@
 namespace Irradiate\Database;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Database\Connectors\ConnectionFactory;
+use Illuminate\Database\Connection as BaseConnection;
 
 class DatabaseServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->bind('db.connection.mysql', '\Irradiate\Database\MySqlConnection');
+        BaseConnection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
+            return new MySqlConnection($connection, $database, $prefix, $config);
+        });
     }
 }
 
